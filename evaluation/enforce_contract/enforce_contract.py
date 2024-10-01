9f8ea383e32159da65bc10bb8b606eb7e8a5f6c9
matt-heery <matt.heery@digital.justice.gov.uk>
2024-10-01 14:06:57 +0100

    working pipeline

--- evaluation/enforce_contract/enforce_contract.py
+++ evaluation/enforce_contract/enforce_contract.py
@@ -1,5 +1,8 @@
 import dlt
+from dlt.common.destination.reference import TDestinationReferenceArg
+from dlt.common.schema.exceptions import DataValidationError
 from dlt.common.typing import TDataItem
+from dlt.pipeline.exceptions import PipelineStepFailed
 
 
 @dlt.resource(write_disposition="append")
@@ -45,9 +48,24 @@ data_pipeline_with_schema = dlt.pipeline(
 )
 
 
+def try_run_pipeline_else_fail(
+    pipeline: dlt.Pipeline, data, fail_destination: TDestinationReferenceArg
+):
+    try:
+        load_info = pipeline.run(data, schema_contract={"tables": "freeze"})
+    except (DataValidationError, PipelineStepFailed) as e:
+        print(f"{e}: {pipeline.pipeline_name} failed, writing to fail destination.")
+        load_info = pipeline.run(data, destination=fail_destination)
+    return load_info
+
+
 if __name__ == "__main__":
     first_load_info = data_pipeline.run(load_data_from_source())
     print(first_load_info)
-    second_load_info = data_pipeline_with_schema.run(
-        load_incorrect_data_from_source(), schema_contract={"tables": "freeze"}
+    second_load_info = try_run_pipeline_else_fail(
+        data_pipeline_with_schema,
+        load_incorrect_data_from_source(),
+        dlt.destinations.filesystem(
+            bucket_url="./evaluation/enforce_contract/fail_data"
+        ),
     )
