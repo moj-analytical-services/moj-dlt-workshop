f117eaabe51a171e3d4f6542922f9a677dcd7004
SoumayaMauthoorMOJ <soumaya.mauthoor@digital.justice.gov.uk>
2024-10-10 15:15:35 +0100

    update boto3 test

--- evaluation/scaling/boto3_test.py
+++ evaluation/scaling/boto3_test.py
@@ -2,34 +2,31 @@ import awswrangler as wr
 import datetime
 import os
 from pprint import pprint
-# scale = 1
-
-# current_time = datetime.datetime.now()
-
-# df = wr.s3.read_parquet(path=f"s3://tpcds-ireland/tpcds/scale={scale}/table=store_sales/")
-
-# print(df.head())
-
-# df.to_parquet(path=f"./tpcds-local/boto/scale={scale}/data.parquet")
-
-# print(datetime.datetime.now() - current_time)
+import itertools
+import shutil
+import pandas as pd
 
 times = {}
 
-scales = [1,2,500,1000]
+scales = [1]
 
 for i in scales:
     current_time = datetime.datetime.now()
-    dfs = wr.s3.read_parquet(path=f"s3://tpcds-ireland/tpcds/scale={i}/table=store_sales/",chunked=True)
+    dfs = wr.s3.read_parquet(path=f"s3://tpcds-ireland/tpcds/scale={i}/table=store_sales/",
+                             chunked=True,
+                             validate_schema=True
+                             )
     dir = f'./tpcds-local/boto/scale={i}/'
-    if not os.path.exists(dir):
-        os.mkdir(dir)
-    data_path = f'./tpcds-local/boto/scale={i}/data.parquet'
-    if os.path.exists(data_path):
-        os.remove(data_path)
-    for df in dfs[:2]:
+    if os.path.exists(dir):
+        shutil.rmtree(dir)
+    os.mkdir(dir)
+    # for n, df in enumerate(itertools.islice(dfs,2)):
+    for n, df in enumerate(dfs):
+        data_path = f'./tpcds-local/boto/scale={i}/data_{n}.parquet'
         df.to_parquet(path=data_path)
+        print(f"chunk {n}")
     times[i] = (datetime.datetime.now() - current_time)
     print(times[i])
 
-pprint(times)
+output = pd.read_parquet(path=dir, columns=['ss_sold_date_sk'])
+print(output.shape)
