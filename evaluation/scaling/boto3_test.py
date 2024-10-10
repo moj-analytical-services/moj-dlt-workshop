57b2af19c8cc8b302c51e660fb9f0b6b71cc102b
SoumayaMauthoorMOJ <soumaya.mauthoor@digital.justice.gov.uk>
2024-10-10 14:59:12 +0100

    update bot3 test

--- evaluation/scaling/boto3_test.py
+++ evaluation/scaling/boto3_test.py
@@ -20,11 +20,16 @@ scales = [1,2,500,1000]
 
 for i in scales:
     current_time = datetime.datetime.now()
-    df = wr.s3.read_parquet(path=f"s3://tpcds-ireland/tpcds/scale={i}/table=store_sales/")
-    print(df.head())
-    os.mkdir(f'./tpcds-local/boto/scale={i}/')
-    os.remove(f'./tpcds-local/boto/scale={i}/data.parquet')
-    df.head().to_parquet(path=f"./tpcds-local/boto/scale={i}/data.parquet")
+    dfs = wr.s3.read_parquet(path=f"s3://tpcds-ireland/tpcds/scale={i}/table=store_sales/",chunked=True)
+    dir = f'./tpcds-local/boto/scale={i}/'
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+    data_path = f'./tpcds-local/boto/scale={i}/data.parquet'
+    if os.path.exists(data_path):
+        os.remove(data_path)
+    for df in dfs[:2]:
+        df.to_parquet(path=data_path)
     times[i] = (datetime.datetime.now() - current_time)
+    print(times[i])
 
 pprint(times)
