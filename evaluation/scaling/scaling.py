cda6786135aa6e0f96530cb8e52e6828424d405a
matt-heery <116661071+matt-heery@users.noreply.github.com>
2024-10-11 12:13:09 +0100

    commiting everythign else

--- evaluation/scaling/scaling.py
+++ evaluation/scaling/scaling.py
@@ -3,15 +3,27 @@ from itertools import islice
 import dlt
 from dlt.destinations import filesystem as destination_filesystem
 from dlt.sources.filesystem import filesystem, read_parquet
-
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import psutil
 scale = 1
+ 
 
-fs_resource = dlt.resource(
-    filesystem(
+def our_chunk(chunk):
+    return chunk  
+
+# Function to calculate optimal chunk size based on memory
+def get_optimal_chunk_size():
+    available_memory = psutil.virtual_memory().available
+    # This is where we adjust
+    return min(max(int(available_memory / (1 * 1024 * 1024)), 10_000), 100_000)
+ 
+optimal_chunk_size = get_optimal_chunk_size()
+print(f"{optimal_chunk_size=}")
+
+dlt_source = dlt.resource(filesystem(
         bucket_url=f"s3://tpcds-ireland/tpcds/scale={scale}/table=store_sales/",
         file_glob="*.parquet",
-    ) | read_parquet()
-)
+    ) | read_parquet(chunksize=optimal_chunk_size), parallelized=True)
 
 dlt_destination = destination_filesystem(bucket_url=f"./tpcds-local/dlt/scale={scale}/")
 
@@ -23,6 +35,6 @@ dlt_pipeline = dlt.pipeline(
     progress='tqdm'
 )
 
-load_info = dlt_pipeline.run(fs_resource)
+load_info = dlt_pipeline.run(dlt_source, loader_file_format="parquet")
 
 print(load_info)
